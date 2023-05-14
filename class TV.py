32bc4f8fc369c24e80f26a5c68abe6bb56f83c0f
Bhea <bheaisipnobleza@gmail.com>
2023-05-14 17:31:39 +0800

    pseudocode

---
+++ class TV.py
@@ -0,0 +1,18 @@
+# Create a class named class TV
+    # The Attributes
+    # Constructors
+    # Methods
+    # Turn on the TV
+    # Turn off the TV
+    # Return the channel
+    # Set new channel
+    # Return the volume
+    # Set new volume
+    # Increase the channel by 1
+    # Decrease the channel by 1
+    # Increase volume by 1
+    # Decrease volume by 1
+# Create a Test driver program named test_tv
+    # first object from class TV
+    # second object from class TV
+# Call the function
