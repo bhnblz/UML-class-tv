f389c3f6ae9d845747bfa9a14fee5230305e7294
Bhea <bheaisipnobleza@gmail.com>
2023-05-16 17:21:10 +0800

    fixing error in the separated file named test_tv

--- test_tv.py
+++ test_tv.py
@@ -1,13 +1,11 @@
-import tv_class
-
-tv_class.TV
+from class_tv import TV
 
 def test_tv():
     # first object from class TV
-    test_tv_1 = tv_class(channel = 30, volume = 3)
-    print("tv1's channel is " + str(test_tv_1), "and volume level is " + str(test_tv_1))
+    test_tv_1 = TV(channel = 30, volume_level = 3)
+    print(f"tv1's channel is " + str(test_tv_1.channel), "and volume level is " + str(test_tv_1.get_volume()))
     # second object from class TV
-    test_tv_2 = tv_class(channel = 3, volume = 2)
-    print("tv2's channel is " + str(test_tv_2), "and volume level is " + str(test_tv_2))
+    test_tv_2 = TV(channel = 3, volume_level = 2)
+    print(f"tv2's channel is " + str(test_tv_2.channel), "and volume level is " + str(test_tv_2.get_volume()))
 # Call the function
 test_tv()
