646757916d2afecd0f6b332d609d2c7b3a7d90a3
Bhea <bheaisipnobleza@gmail.com>
2023-05-16 21:59:04 +0800

    applied python module

--- test_tv.py
+++ test_tv.py
@@ -1,11 +1,21 @@
-from class_tv import TV
+import pyfiglet
+from colorama import Fore
+greetings = "Kon'nichiwa!"
+print(pyfiglet.figlet_format (greetings, font = "standard"))
+introduction = "Welcome to this file! This is a separate file from class_tv file. This program will display the required output."
+print(Fore.LIGHTRED_EX + "=" * 113)
+print(Fore.LIGHTRED_EX + introduction.center(113))
+print("=" * 113)
+
+# Import class tv from TV
+from class_tv import TV 
 
 def test_tv():
     # first object from class TV
     test_tv_1 = TV(channel = 30, volume_level = 3, tv_is_on = True)
-    print("tv1's channel is ", (test_tv_1.channel), "and volume level is ", (test_tv_1.get_volume()))
+    print(Fore.LIGHTMAGENTA_EX + "\ntv1's channel is ", (test_tv_1.channel), "and volume level is ", (test_tv_1.get_volume()))
     # second object from class TV
     test_tv_2 = TV(channel = 3, volume_level = 2, tv_is_on = True)
-    print("tv2's channel is ", (test_tv_2.channel), "and volume level is ", (test_tv_2.get_volume()))
-# Call the function
+    print(Fore.LIGHTMAGENTA_EX + "\ntv2's channel is ", (test_tv_2.channel), "and volume level is ", (test_tv_2.get_volume()))
+# Call the function 
 test_tv()
